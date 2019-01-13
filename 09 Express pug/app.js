070afddc39f22a14c7104726e6b599dedf10d281
mathiasuy <mathias.battistella@fing.edu.uy>
2019-01-13 15:54:13 -0300

    bucles con pug

---
+++ 09 Express pug/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var fs = require('fs');
+const pug = require('pug');
+
+
+var app = express();
+app.use(express.static(__dirname + '/public')); // para indicar cual sera la carpeta root www
+
+app.get('/', (peticion, respuesta) => {
+	respuesta.render('index.pug', {
+		titulo : "este es el nombre de mi app",
+		textoParrafo : "Nuestra tienda de camisetas ofrece lo mejor, satisfacción 100% garantizada"
+	});
+})
+
+app.listen(3000, ()=>{
+	console.log('Escuchando en el puerto 3000');
+})
