8f864a9e4156aa8a55170df7175913d6a95b1774
mathiasuy <mathias.battistella@fing.edu.uy>
2019-01-12 21:27:41 -0300

    Escribiendo un registro.txt

--- 04 Cambiando el HTML/servidor3.js
+++ 04 Cambiando el HTML/servidor3.js
@@ -1,6 +1,6 @@
 var servidor = require('http'); //modulo http que contiene todos los metodos para hacer funcinoar un servidor web a traves de http
 var url = require('url');
-
+var fs = require('fs'); //file system de nodejs, preparo al servidor para poder leer y escribir archivos
 
 function iniciar(enrutar, manejador){// recibo la funcion que esta en enrutador.js que me pasaron en inicio.js 
 	function arrancaServidor(requiere, respuesta){
@@ -10,6 +10,9 @@ function iniciar(enrutar, manejador){// recibo la funcion que esta en enrutador.
 		//ese mensaje aparecerá 2 veces en la consola, porque el navegador suele enviar 2 peticiones, una para el favicon y otra para la página web en sí.
 
 		var contenido = enrutar(ruta, manejador);	//le paso a la funcion el path de la url donde estoy parado
+
+		var registro = fs.createWriteStream('registro.txt',{'flags':'a'});//pongo una bandera para indicar que quiero agregar (append) contenido
+		registro.write(ruta + '\n');
 
 		respuesta.writeHead(200, {"Content-Type":"text/html"});
 		respuesta.write("<h1>"+ contenido +"</h1>"); // Se verá en el navegador
