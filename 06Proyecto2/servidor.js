a402cb4462938ef58255ccf0f63433c977f44652
mathiasuy <mathias.battistella@fing.edu.uy>
2019-01-13 13:20:07 -0300

    servidor base sin express v2

---
+++ 06Proyecto2/servidor.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer((peticion, respuesta) =>{
+
+	respuesta.writeHead(200, {'Content-Type':'text/html'});
+
+	switch(peticion.url){
+		case '/':
+			plantilla = 'inicio.html';
+			break;
+		case '/nodejs':
+			plantilla = "sobrenode.html";
+			break;
+		default:
+			plantilla = "404.html";
+			break;
+	}
+
+	fs.readFile("./plantillas/" + plantilla, (error, datos) =>{
+		respuesta.write(datos);
+		respuesta.end();
+	})
+
+
+}).listen(3000, 'localhost');
